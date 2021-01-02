15ad11a5dc321ecbd014dc6a5650ea27dac712a5
DanaAltinawi <76616637+DanaAltinawi@users.noreply.github.com>
2021-01-02 20:29:39 +0300

    CW5

---
+++ q1.js
@@ -0,0 +1,12 @@
+// This is the answer of question1 
+
+
+let myAge= 25;
+let MyNewAge= 5*myAge;
+console.log(myAge);
+console.log(5*myAge);
+console.log(`حين أبلغ {MyNewAge}$ سيبدأ زحل باستقبال المبرمجين على سطحه 🪐`);
+
+let numberOfYears= 2021-1969;
+console.log(`هو عدد الأعوام التي مرت على صعود الإنسان على سطح القمر ${numberOfYears}🌑`+ `حين أبلغ {MyNewAge}$ سيبدأ زحل باستقبال المبرمجين على سطحه 🪐`);
+// <!-- This is the answer of question1 هذه اجابة السؤال الاول -->
